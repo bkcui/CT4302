66885aed04ae43f18540bef6ce9904e7aa5061ff
bkcui <juno@gist.ac.kr>
2018-10-23 07:13:30 +0900

    Add files via upload
    fix

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true })); //
 var fs = require('fs');
 var what = JSON.parse(fs.readFileSync('what.json', 'utf8'));
 var why = JSON.parse(fs.readFileSync('why.json', 'utf8'));
+var why_neg = JSON.parse(fs.readFileSync('why_neg.json', 'utf8'));
 var yno = JSON.parse(fs.readFileSync('YN.json', 'utf8'));
 var how = JSON.parse(fs.readFileSync('how.json', 'utf8'));
+var how_neg = JSON.parse(fs.readFileSync('how_neg.json', 'utf8'));
 var diff = JSON.parse(fs.readFileSync('diff.json', 'utf8'));
 
 console.log('Answer: ' + what['짜장면']['자장면']['올바']); //// debugging
@@ -56,217 +58,40 @@ app.post('/', function (request, response) {
     var NNP = request.body.queryResult.parameters['NNP'];
     var NNM = request.body.queryResult.parameters['NNM'];
     var NNG = request.body.queryResult.parameters['NNG'];
+    var NNG1 = request.body.queryResult.parameters['NNG1'];
     var MD  = request.body.queryResult.parameters['MD'];
     var MAG = request.body.queryResult.parameters['MAG'];
     var NP  = request.body.queryResult.parameters['NP']; //                  here
     
     let action = (request.body.queryResult.action) ? request.body.queryResult.action: 'default';
     
-    
-    
     const actionHandlers = {
-        'NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: yno[NNG][NNG][NNG]};
+        'diff': () => {
+            let responseToUser = { fulfillmentText: diff[NNG][NNG1]};
             sendResponse(responseToUser);
         },
-
-        'NNG.NNG.VXV': () => {
-            let responseToUser = { fulfillmentText: yno[NNG][NNG][VXV]};
-            sendResponse(responseToUser);
-        },
-        
-        'NNG.MAG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][MAG]};
-            sendResponse(responseToUser);
-        },
-
-        'MAG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: why[MAG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.VV': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][VV]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.VA.NNG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][VA][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.NNG.VA': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][NNG][NNG][VA]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.VA': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][VA]};
-            sendResponse(responseToUser);
-        },
-
-        'VA.VV.NNG': () => {
-            let responseToUser = { fulfillmentText: why[VA][VV][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.VA': () => {
-            let responseToUser = { fulfillmentText: why[NNG][VA]};
-            sendResponse(responseToUser);
-        },
-
-        'MD.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: why[MD][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.VV': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][NNG][VV]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.VV.NNG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][VV][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.MAG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][MAG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.VV': () => {
-            let responseToUser = { fulfillmentText: why[NNG][VV]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.VXV': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][VXV]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.VA': () => {
-            let responseToUser = { fulfillmentText: why[NNG][NNG][NNG][VA]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.VA.NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: why[NNG][VA][NNG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'XR.NNG.VV': () => {
-            let responseToUser = { fulfillmentText: why[XR][NNG][VV]};
-            sendResponse(responseToUser);
-        },
-        
-        'NNG.NNG.VA': () => {
-            let responseToUser = { fulfillmentText: what[NNG][NNG][VA]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: what[NNG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.NP': () => {
-            let responseToUser = { fulfillmentText: what[NNG][NNG][NNG][NP]};
-            sendResponse(responseToUser);
-        },
-
-        'NNP.NNG.VV': () => {
-            let responseToUser = { fulfillmentText: what[NNP][NNG][VV]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: what[NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.MAG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: what[NNG][MAG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.MAG.NNG': () => {
-            let responseToUser = { fulfillmentText: what[NNG][NNG][MAG][NNG]};
-            sendResponse(responseToUser);
-        },
-        
-        'NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: how[NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.VA.NNG': () => {
-            let responseToUser = { fulfillmentText: how[NNG][NNG][VA][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.VV': () => {
+        'how': () => {
             let responseToUser = { fulfillmentText: how[NNG][VV]};
             sendResponse(responseToUser);
         },
-
-        'NNG.VA.VV': () => {
-            let responseToUser = { fulfillmentText: how[NNG][VA][VV]};
+        'how_neg': () => {
+            let responseToUser = { fulfillmentText: how_neg[NNG][VV][VXV]};
             sendResponse(responseToUser);
         },
-
-        'NNG.VV.MAG.VV': () => {
-            let responseToUser = { fulfillmentText: how[NNG][VV][MAG][VV]};
+        'what': () => {
+            let responseToUser = { fulfillmentText: what[NNG][NNG1][NP]};
             sendResponse(responseToUser);
         },
-
-        'NNG.VV.VV': () => {
-            let responseToUser = { fulfillmentText: how[NNG][VV][VV]};
+        'why': () => {
+            let responseToUser = { fulfillmentText: why[NNG][MAG][NNG1]};
             sendResponse(responseToUser);
         },
-
-        'NNG.NNG.VV': () => {
-            let responseToUser = { fulfillmentText: how[NNG][NNG][VV]};
+        'why_neg': () => {
+            let responseToUser = { fulfillmentText: why_neg[NNG][MAG][NNG1][VV][VXV]};
             sendResponse(responseToUser);
         },
-
-        'NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: how[NNG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: how[NNG][NNG][NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.MAG': () => {
-            let responseToUser = { fulfillmentText: how[NNG][MAG]};
-            sendResponse(responseToUser);
-        },
-        
-        'NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: diff[NNG][NNG]};
-            sendResponse(responseToUser);
-        },
-
-        'NNP.NNP': () => {
-            let responseToUser = { fulfillmentText: diff[NNP][NNP]};
-            sendResponse(responseToUser);
-        },
-
-        'NNG.NNG.NNG.NNG': () => {
-            let responseToUser = { fulfillmentText: diff[NNG][NNG][NNG][NNG]};
+        'YN': () => {
+            let responseToUser = { fulfillmentText: yno[NNG][NNG1][VV]};
             sendResponse(responseToUser);
         },
         
@@ -275,12 +100,6 @@ app.post('/', function (request, response) {
             sendResponse(responseToUser);
         }
     };
-
-    if (!actionHandlers[action]) {
-         action = 'default';
-    }
-
-    actionHandlers[action]();
     
     function sendResponse(responseToUser) {
         if (typeof responseToUser === 'string') {
